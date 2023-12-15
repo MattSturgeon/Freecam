95215735030e253790b4bbb7d5fbe2a855a5351b
Matt Sturgeon <matt@sturgeon.me.uk>
2023-12-15 15:04:16 +0000

    Add forge init code

--- src/lexforge/java/net/xolt/freecam/forge/FreecamLexForge.java
+++ src/lexforge/java/net/xolt/freecam/forge/FreecamLexForge.java
@@ -1,19 +1,29 @@
 package net.xolt.freecam.forge;
 
-import net.xolt.freecam.Freecam;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import dev.architectury.platform.forge.EventBuses;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.xolt.freecam.Freecam;
+import net.xolt.freecam.config.ModConfig;
 
 @Mod("freecam")
 public class FreecamLexForge {
 
     public FreecamLexForge() {
-    }
+        // Register our event bus with Architectury
+        EventBuses.registerModEventBus(Freecam.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
 
-    @SubscribeEvent
-    public void onInit(FMLCommonSetupEvent event) {
+        // Register our config screen with Forge
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((client, parent) ->
+                AutoConfig.getConfigScreen(ModConfig.class, parent).get()
+        ));
+
+        // Call our init
+        // TODO split init & early init?
+        //      forge wants us to do most of our init in #onInit()
         Freecam.init();
     }
-
 }
--- src/neoforge/java/net/xolt/freecam/forge/FreecamNeoForge.java
+++ src/neoforge/java/net/xolt/freecam/forge/FreecamNeoForge.java
@@ -1,19 +1,24 @@
 package net.xolt.freecam.forge;
 
-import net.xolt.freecam.Freecam;
-import net.neoforged.bus.api.SubscribeEvent;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.client.ConfigScreenHandler;
+import net.xolt.freecam.Freecam;
+import net.xolt.freecam.config.ModConfig;
 
 @Mod("freecam")
 public class FreecamNeoForge {
 
     public FreecamNeoForge() {
-    }
+        // Register our config screen with Forge
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((client, parent) ->
+                AutoConfig.getConfigScreen(ModConfig.class, parent).get()
+        ));
 
-    @SubscribeEvent
-    public void onInit(FMLCommonSetupEvent event) {
+        // Call our init
+        // TODO split init & early init?
+        //      forge wants us to do most of our init in #onInit()
         Freecam.init();
     }
-
 }
