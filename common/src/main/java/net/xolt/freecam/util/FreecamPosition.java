a1d2dba13d49633a1000f0e31a0c75109dbed6e0
Matt Sturgeon <matt@sturgeon.me.uk>
2024-01-09 03:13:47 +0000

    Rollback to 1.19.2

--- common/src/main/java/net/xolt/freecam/util/FreecamPosition.java
+++ common/src/main/java/net/xolt/freecam/util/FreecamPosition.java
@@ -1,10 +1,10 @@
 package net.xolt.freecam.util;
 
+import com.mojang.math.Quaternion;
+import com.mojang.math.Vector3f;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.level.ChunkPos;
-import org.joml.Quaternionf;
-import org.joml.Vector3f;
 
 public class FreecamPosition {
     public double x;
@@ -13,7 +13,7 @@ public class FreecamPosition {
     public float pitch;
     public float yaw;
 
-    private final Quaternionf rotation = new Quaternionf(0.0F, 0.0F, 0.0F, 1.0F);
+    private final Quaternion rotation = new Quaternion(0.0F, 0.0F, 0.0F, 1.0F);
     private final Vector3f verticalPlane = new Vector3f(0.0F, 1.0F, 0.0F);
     private final Vector3f diagonalPlane = new Vector3f(1.0F, 0.0F, 0.0F);
     private final Vector3f horizontalPlane = new Vector3f(0.0F, 0.0F, 1.0F);
@@ -25,14 +25,17 @@ public class FreecamPosition {
         setRotation(entity.getYRot(), entity.getXRot());
     }
 
-    // From net.minecraft.client.render.Camera.setRotation
+    // From net.minecraft.client.Camera.setRotation
     public void setRotation(float yaw, float pitch) {
         this.pitch = pitch;
         this.yaw = yaw;
-        rotation.rotationYXZ(-yaw * ((float) Math.PI / 180), pitch * ((float) Math.PI / 180), 0.0f);
-        horizontalPlane.set(0.0f, 0.0f, 1.0f).rotate(rotation);
-        verticalPlane.set(0.0f, 1.0f, 0.0f).rotate(rotation);
-        diagonalPlane.set(1.0f, 0.0f, 0.0f).rotate(rotation);
+        rotation.set(-yaw * ((float) Math.PI / 180), pitch * ((float) Math.PI / 180), 0.0f, 1.0f);
+        horizontalPlane.set(0.0f, 0.0f, 1.0f);
+        horizontalPlane.transform(rotation);
+        verticalPlane.set(0.0f, 1.0f, 0.0f);
+        verticalPlane.transform(rotation);
+        diagonalPlane.set(1.0f, 0.0f, 0.0f);
+        diagonalPlane.transform(rotation);
     }
 
     // Invert the rotation so that it is mirrored
