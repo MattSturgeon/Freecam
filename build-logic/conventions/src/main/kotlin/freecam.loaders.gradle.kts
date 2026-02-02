ec7f95f601a2a3a967787ee3a334c8e126a08ef2
Matt Sturgeon <matt@sturgeon.me.uk>
2026-02-02 08:20:36 +0000

    Simplify common → loader dependency
    We can use the build classes; this is semantically the same as using the
    pre-processed sources, but without re-compilation.
    
    This is much simpler and more idiomatic.
    
    # Conflicts:
    #	build-logic/conventions/src/main/kotlin/freecam.loaders.gradle.kts

--- build-logic/conventions/src/main/kotlin/freecam.loaders.gradle.kts
+++ build-logic/conventions/src/main/kotlin/freecam.loaders.gradle.kts
@@ -3,15 +3,8 @@ plugins {
     id("freecam.publish")
 }
 
-sourceSets {
-    main {
-        // Manually depend on common's pre-processed sources.
-        // NOTE: loaders have no build dependency on common, so API/implementation
-        //  classes and transitive dependencies must be manually propagated.
-        val commonTasks = commonNode.project.tasks
-        java.srcDirs(commonTasks.named<Sync>("stonecutterGenerate").map { it.destinationDir })
-        resources.srcDirs(commonTasks.processResources.map { it.destinationDir })
-    }
+dependencies {
+    implementation(commonNode.project)
 }
 
 publishing {
